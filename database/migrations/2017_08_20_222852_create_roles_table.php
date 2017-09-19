9a354ddd4ae57a378cddf02c0658e9bb625cf965
John Dinsmore <djibba22@fullsail.edu>
2017-09-19 16:44:58 -0400

    comment out migrations

--- database/migrations/2017_08_20_222852_create_roles_table.php
+++ database/migrations/2017_08_20_222852_create_roles_table.php
@@ -4,30 +4,30 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('description');
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('roles');
-    }
-}
+//class CreateRolesTable extends Migration
+//{
+//    /**
+//     * Run the migrations.
+//     *
+//     * @return void
+//     */
+//    public function up()
+//    {
+//        Schema::create('roles', function (Blueprint $table) {
+//            $table->increments('id');
+//            $table->string('name');
+//            $table->string('description');
+//            $table->timestamps();
+//        });
+//    }
+//
+//    /**
+//     * Reverse the migrations.
+//     *
+//     * @return void
+//     */
+//    public function down()
+//    {
+//        Schema::dropIfExists('roles');
+//    }
+//}
